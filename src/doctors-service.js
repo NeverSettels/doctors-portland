ac7229204f360dfd9d670a1328ee3d36f683711c
NeverSettels <settels.eric@gmail.com>
2020-02-14 15:26:32 -0800

    Add status 200 check on api call and error handling also add clearing of search on every search

--- src/doctors-service.js
+++ src/doctors-service.js
@@ -5,27 +5,38 @@ export class DoctorsApi {
   async getDoctorList(isNameBool, name) {
     let nameAPIUrl = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5051%2C-122.6750%2C100&user_location=45.5051%2C-122.6750&skip=0&limit=10&user_key=93de781a4625762135fb1a235aab4117`
     let symtomsAPIUrl = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.5051,-122.6750,100&skip=2&limit=10&user_key=93de781a4625762135fb1a235aab4117`
-    let body = {};
+    let body;
+    let response;
     try {
       if (isNameBool) {
-        let response = await fetch(nameAPIUrl);
-        body = await response.json();
+        response = await fetch(nameAPIUrl);
+        //  body = await response.json();
       } else {
-        let response = await fetch(symtomsAPIUrl);
-        body = await response.json();
+        response = await fetch(symtomsAPIUrl);
+        //  body = await response.json();
       }
-      let { data } = body
-      data.forEach(doctor => {
-        let tempObj = {};
-        let { profile: { first_name, middle_name, last_name }, practices } = doctor;
-        let { accepts_new_patients, visit_address: { city, street, zip, state }, phones } = practices[0];
-        tempObj = { first_name, middle_name, last_name, accepts_new_patients, city, street, zip, state, phones };
-        this.doctors.push(tempObj)
-      })
-      return this.doctors
+      if (response)
+        if (response.ok && response.status == 200) {
+          body = await response.json();
+          let { data } = body
+          data.forEach(doctor => {
+            let tempObj = {};
+            let { profile: { first_name, middle_name, last_name }, practices } = doctor;
+            let { accepts_new_patients, visit_address: { city, street, zip, state }, phones } = practices[0];
+            tempObj = { first_name, middle_name, last_name, accepts_new_patients, city, street, zip, state, phones };
+            this.doctors.push(tempObj)
+          })
+          return this.doctors
+        } else {
+          return false;
+        }
+
     }
     catch (error) {
       console.error("ERROR ERROR!" + error.message)
     }
   }
+}
+function checkResponse(response) {
+
 }
--- src/main.js
+++ src/main.js
@@ -10,29 +10,33 @@ let doctors = new Doctors()
 $(document).ready(() => {
   $('#nameForm').submit(event => {
     event.preventDefault();
+    $('#found').empty()
     let name = $('#name').val();
     (async () => {
       let api = new DoctorsApi(doctors);
       doctors.doctorsList = await api.getDoctorList(true, name);
-      if (doctors.doctorsList.length < 1) {
+      if (!doctors.doctorsList) {
+        $('#error').removeClass('no-display')
+      } else if (doctors.doctorsList.length < 1) {
         $('#no-data').removeClass('no-display')
-      }
-      console.log(doctors)
-      doctors.doctorsList.forEach((doctor, i) => {
-        let { first_name, middle_name, last_name, accepts_new_patients, city, street, zip, state, phones } = doctor;
-        $('#found').append(`
+      } else {
+        console.log(doctors)
+        doctors.doctorsList.forEach((doctor, i) => {
+          let { first_name, middle_name, last_name, accepts_new_patients, city, street, zip, state, phones } = doctor;
+          $('#found').append(`
         <div>
-          <h2>DR. ${first_name} ${middle_name} ${last_name}</h2>
+          <h2>DR. ${first_name} ${middle_name ? middle_name : ''} ${last_name}</h2>
           <h3>Accepts new patients: ${ accepts_new_patients ? 'Yes' : 'No'}.</h3>
           <h3>Adress: ${street}, ${city}, ${state}, ${zip}. </h3>
           <div id="phones${i}">
           </div>
         </div>
         `);
-        phones.forEach(phone => {
-          $(`#phones${i}`).append(`<h6>Phone: ${phone.number}: Type: ${phone.type} </h6>`);
-        })
-      })
+          phones.forEach(phone => {
+            $(`#phones${i}`).append(`<h6>Phone: ${phone.number}: Type: ${phone.type} </h6>`);
+          });
+        });
+      }
     })();
   })
 })
