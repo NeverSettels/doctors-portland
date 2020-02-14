b29f638d0d3bf599bb54d177ca57c1e77db9cb8b
NeverSettels <settels.eric@gmail.com>
2020-02-14 09:58:19 -0800

    Installed .env dependency but having to with process.env.API_KEY returning as undefined so im incluing the api key cf

--- src/doctors-service.js
+++ src/doctors-service.js
@@ -4,7 +4,7 @@ export class Doctors {
 
   async getDoctorList() {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=93de781a4625762135fb1a235aab4117`);
       let body = await response.json();
       console.log(body);
 
--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Doctors } from './doctors-service'
+$(document).ready(() => {
 
+  (async () => {
+    let doctors = new Doctors()
+    doctors.getDoctorList()
+  })();
+})
