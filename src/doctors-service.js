5552d7a60fdacf0e00d28d60f41954ffc9563863
NeverSettels <settels.eric@gmail.com>
2020-02-14 09:19:25 -0800

    Add webpack config sincr i forgot it and spent way tolong trying to figure out with my code was broken

--- src/doctors-service.js
+++ src/doctors-service.js
@@ -6,6 +6,8 @@ export class Doctors {
     try {
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}`);
       let body = await response.json();
+      console.log(body);
+
     }
     catch (error) {
       console.error("ERROR ERROR!" + error.message)
